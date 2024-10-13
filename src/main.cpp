feee47d83621ae129fb68c5786c41637209d9ddc
Nicolo Dellepiane <nicodelle04@gmail.com>
2024-10-14 00:18:02 +0200

    added functioning model

--- src/main.cpp
+++ src/main.cpp
@@ -3,34 +3,25 @@
 #include <PDM.h>
 
 #include <ArduTFLite.h>
-#include "jarvis025.h" // here the model (jarvis 0.1.tflite) is compiled into a matrix, to be used by the interpreter to run inference. To see how the model was obtained, check the notebooks! 
+#include "jarvis026.h" 
 #include "hcData.h"
 
-#define DATA RDj097
+#define DATA unknown_66_2
 
-
-void timeAndPredict()
+float runInference(float data[8000])
 {
-  int t0 = millis();
-  if (!modelRunInference())
+  for (int i = 0; i < 8000; i++)
   {
-    Serial.println("RunInference Failed!");
-    return;
+      modelSetInput(data[i], i); //the input matrix is flattened to a 1D, 64*16 array
   }
-
-  // Run inference on the given data
-  float y = modelGetOutput(0);
-  int t1 = millis();
-  Serial.print("Output: ");
-  Serial.println(y);
-  Serial.print("Time taken: ");
-  Serial.print(t1 - t0);
-  Serial.println(" ms");
+  modelRunInference();
+  return modelGetOutput(0);
 }
 
 //define an area inside memory for the model to perform undisutrbed operations in
 constexpr int kTensorArenaSize = 70000;
 alignas(16) uint8_t tensor_arena[kTensorArenaSize];
+
 
 //all code to be executed once goes here
 void setup()
@@ -41,26 +32,16 @@ void setup()
     ;
 
   Serial.println("Initializing TensorFlow Lite Micro Interpreter...");
-  if (!modelInit(utils_models_jarvis0_2_5_tflite, tensor_arena, kTensorArenaSize))
+  if (!modelInit(utils_models_jarvis0_2_6_tflite, tensor_arena, kTensorArenaSize))
   {
     Serial.println("Model initialization failed!");
     while (true)
       ;
   }
 
-  //Place all input data inside the input tensor of the model, using the helper function
-  for (int i = 0; i < 8000; i++)
-  {
-      modelSetInput(DATA[i], i); //the input matrix is flattened to a 1D, 64*16 array
-  }
-
-  Serial.println("Model initialization done.");
 }
 
 void loop()
 {
-  timeAndPredict();
-  while (1 == 1)
-  {
-  }
+  runInference(DATA);
 }
