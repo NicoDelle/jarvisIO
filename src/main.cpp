44df3d4cd49ca5dd6dbd57ec098473776df1a6d3
Nicolo Dellepiane <nicodelle04@gmail.com>
2024-10-18 10:43:33 +0200

    semi-working audioProvider

--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,40 @@
 #include <WiFiNINA.h>
 #include <PDM.h>
 
+#include "audioProvider.h"
+
+/**
 #include <ArduTFLite.h>
 #include "jarvis026.h" 
-#include "hcData.h"
+*constexpr int kTensorArenaSize = 70000;
+*alignas(16) uint8_t tensor_arena[kTensorArenaSize];
+*float data[SR];
 
-#define DATA unknown_66_2
-
-float runInference(float data[8000])
+float runInference(float data[SR])
 {
-  for (int i = 0; i < 8000; i++)
+  for (int i = 0; i < SR; i++)
   {
       modelSetInput(data[i], i); //the input matrix is flattened to a 1D, 64*16 array
   }
   modelRunInference();
   return modelGetOutput(0);
 }
+*/
 
-//define an area inside memory for the model to perform undisutrbed operations in
-constexpr int kTensorArenaSize = 70000;
-alignas(16) uint8_t tensor_arena[kTensorArenaSize];
+int buttonPin = 2;
+char ssid[6] = "delle";
+char pass[12] = "Iusearchbtw";
+char servername[] = "192.168.15.2";
+int serverport = 2023;
+WiFiClient client;
 
+//sends all data in the given buffer to the configured destination
+void sendData(WiFiClient& client, AudioProvider& audioProvider)
+{
+
+  // Print a sample on serial
+  client.write((uint8_t*) audioProvider.audio, sizeof(float) * SR);
+}
 
 //all code to be executed once goes here
 void setup()
@@ -31,6 +45,7 @@ void setup()
   while (!Serial)
     ;
 
+  /**
   Serial.println("Initializing TensorFlow Lite Micro Interpreter...");
   if (!modelInit(utils_models_jarvis0_2_6_tflite, tensor_arena, kTensorArenaSize))
   {
@@ -38,10 +53,38 @@ void setup()
     while (true)
       ;
   }
+  Serial.println("Model initialized!");
+  */
+  //PDM setup
+  if (!PDMsetup())
+  {
+    while (1) {};
+  }
+  //WiFI setup
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+  if (client.connect(servername, serverport)) {
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
 
+    Serial.println("Setup finished!");
+    AudioProvider audioProvider;
+    sendData(client, audioProvider);
+  } else {
+    Serial.println("Connection to server failed!");
+  }
 }
 
 void loop()
 {
-  runInference(DATA);
+
+  //basta copiare i dati in ordine da un buffer a un array. Probabilmente conviene poi shiftarli e ripiazzare quelli nuovi in coda, ma dipende dai tempi di esecuzione del modello
+
+  
+  //runInference(DATA);
+  return;
 }
--- utils/dataServer.py
+++ utils/dataServer.py
@@ -5,7 +5,7 @@ import wave
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to a specific address and port
-server_address = ('192.168.212.2', 5000) # IP address of your PC
+server_address = ('192.168.15.2', 2023) # IP address of your PC
 sock.bind(server_address)
 
 # Listen for incoming connections
@@ -14,8 +14,8 @@ sock.listen(1)
 # Open a .wav file for writing
 wav_file = wave.open('output.wav', 'wb')
 wav_file.setnchannels(1) # mono audio
-wav_file.setsampwidth(2) # 2 bytes per sample
-wav_file.setframerate(16000) # sample rate
+wav_file.setsampwidth(4) # 2 bytes per sample
+wav_file.setframerate(8000) # sample rate
 
 while True:
     # Wait for a connection
@@ -26,7 +26,7 @@ while True:
 
         # Receive the data in small chunks and write it to the .wav file
         while True:
-            data = connection.recv(16)
+            data = connection.recv(8000 * 4)
             print('received {!r}'.format(data))
             if data:
                 wav_file.writeframes(data)
