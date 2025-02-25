fa700d392e10a6b673a134bc695eabdaffc26919
Nicolo' Dellepiane <nicolo.dellepiane@mail.polimi.it>
2025-02-25 18:20:59 +0100

    implemented feature extraction (features are close to the ones obtained in python

--- lib/melspectrogram/melspectrogram.h
+++ lib/melspectrogram/melspectrogram.h
@@ -5,9 +5,11 @@
 #define N_MELS 32
 #define HOP_LENGTH 400
 #define N_FFT 256
+#define NUM_FRAMES (SAMPLE_RATE / HOP_LENGTH + 1)
 
 float* frame(float *audio, float *buffer, float *end);
 void extractPowerSpectrum(float *audio, float powerSpectrum[SAMPLE_RATE / HOP_LENGTH + 1][N_FFT/2 + 1]);
-void extractMelEnergies(float *audio, float melEnergies[SAMPLE_RATE / HOP_LENGTH + 1][N_MELS]);
-void powerTodB(float melspectrogram[SAMPLE_RATE / HOP_LENGTH + 1][N_MELS]);
-void melspectrogram(float *audio, float melspectrogram[SAMPLE_RATE / HOP_LENGTH + 1][N_MELS]);
+void createMelFilterbank(float filterbank[N_MELS][N_FFT/2 + 1]);
+void extractMelEnergies(float *audio, float melEnergies[N_MELS][NUM_FRAMES], float melFilterbank[N_MELS][N_FFT/2 +1]);
+void powerTodB(float melspectrogram[N_MELS][NUM_FRAMES]);
+void melspectrogram(float *audio, float melspectrogram[N_MELS][NUM_FRAMES], float melFilterbank[N_MELS][N_FFT/2 +1]);
