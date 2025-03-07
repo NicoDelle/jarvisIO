ac5364778040410ab89e4ad46a943ed72cfda175
Nicolo' Dellepiane <nicolo.dellepiane@mail.polimi.it>
2025-03-07 17:42:18 +0100

    trash

---
+++ lib/audioProvider/audioProvider.cpp
@@ -0,0 +1,6 @@
+#include "audioProvider.h"
+
+short *getAudio(short *audio[LEN], short *buffer[BUFFER_LEN], int *sRsead)
+{
+
+}
---
+++ lib/audioProvider/audioProvider.h
@@ -0,0 +1,4 @@
+#define LEN 8000
+#define BUFFER_LEN 8000
+
+short *getAudio(short* audio[LEN], short *buffer[BUFFER_LEN], int *sRead);
